162510ff6f8b55e36bc67724e59fc287d9493aca
David Jagoe <davidjagoe@gmail.com>
2012-01-24 14:12:13 +0000

    Implemented basic print of issues in a milestone.

--- list_issues.py
+++ list_issues.py
@@ -2,6 +2,7 @@
 
 import requests
 import simplejson
+import sys
 
 import config   as c
 import markdown as fmt
@@ -10,38 +11,26 @@ import vars     as v
 ### WRITER
 
 def write_issue(issue):
-    fmt.list_item(issue["title"])
-    fmt.newline()
+    title      = issue["title"]
+    labels_str = ",".join([l["name"] for l in issue["labels"]])
+    body       = issue["body"]
 
-def write_issues(milestone):
-    milestone_number = milestone["number"]
-    issues_url = "{0}?milestone={1}".format(c.get_issues_url(v._CONFIG_), milestone_number)
-    resp = requests.get(issues_url, auth=c.get_basic_auth_credentials(v._CONFIG_))
-    if resp.status_code == 200:
-        issues = simplejson.loads(resp.content)
-        for issue in issues:
-            write_issue(issue)
-    else:
-        raise Exception("Error reading issues. Response: {0}".format(resp.status_code))
-
-def write_milestone(milestone):
-    title = milestone["title"]
-    description = milestone["description"]
-    fmt.h2(title)
-    fmt.paragraph(description)
-    write_issues(milestone)
+    fmt.h2(title + " -- " + labels_str)
+    fmt.paragraph(body)
     fmt.rule()
 
 ### MAIN
 
-def main():
+def main(milestone):
     try:
         c.setup("issues.md")
-        resp = requests.get(c.get_issues_url(v._CONFIG_), auth=c.get_basic_auth_credentials(v._CONFIG_))
+        resp = requests.get(c.get_issues_url(v._CONFIG_), 
+                            auth=c.get_basic_auth_credentials(v._CONFIG_),
+                            params={'milestone': milestone})
         if resp.status_code == 200:
             fmt.h1("Issues")
             issues = simplejson.loads(resp.content)
-            for issue in milestones:
+            for issue in issues:
                 write_issue(issue)
             print "Done."
         else:
@@ -52,4 +41,6 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    # Usage: list_issues.py milestone1 ; milestone is a number
+    milestone = sys.argv[1]
+    main(milestone)
